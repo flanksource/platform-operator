e7a91ca077f22c65d22b4518251380e5990ffbad
Salvatore Mazzarino <dev@mazzarino.cz>
2020-01-09 17:54:15 +0100

    Introduce Kubebuilder

--- main.go
+++ main.go
@@ -1,88 +1,85 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
-
 import (
-	"fmt"
+	"flag"
 	"os"
-	"github.com/moshloop/platform-operator/cmd"
+	"time"
 
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	"github.com/flanksource/platform-operator/pkg/controllers/cleanup"
+	// +kubebuilder:scaffold:imports
 )
 
 var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
+	scheme   = runtime.NewScheme()
+	setupLog = logf.Log.WithName("setup")
 )
 
+func init() {
+	_ = clientgoscheme.AddToScheme(scheme)
+
+	// +kubebuilder:scaffold:scheme
+}
+
 func main() {
-	var root = &cobra.Command{
-		Use: "platform-operator",
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			level, _ := cmd.Flags().GetCount("loglevel")
-			switch {
-			case level > 1:
-				log.SetLevel(log.TraceLevel)
-			case level > 0:
-				log.SetLevel(log.DebugLevel)
-			default:
-				log.SetLevel(log.InfoLevel)
-			}
-		},
-	}
+	var metricsAddr string
+	var enableLeaderElection bool
+	var watchInterval time.Duration
 
-	root.AddCommand(cmd.Run)
-	// root.AddCommand(
-	// 	cmd.Dependencies,
-	// 	cmd.Images,
-	// 	cmd.MachineImages,
-	// 	cmd.Upgrade,
-	// 	cmd.Test,
-	// 	cmd.Build,
-	// 	cmd.Provision,
-	// 	cmd.Cleanup,
-	// 	cmd.Status,
-	// 	cmd.Access,
-	// 	cmd.Deploy,
-	// 	cmd.Harbor,
-	// 	cmd.DNS,
-	// 	cmd.Render)
+	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
+		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	flag.DurationVar(&watchInterval, "watch-interval", 10*time.Minute, "Minimum frequency at which watched resources are reconciled.")
+	flag.Parse()
 
-	if len(commit) > 8 {
-		version = fmt.Sprintf("%v, commit %v, built at %v", version, commit[0:8], date)
-	}
-	root.AddCommand(&cobra.Command{
-		Use:   "version",
-		Short: "Print the version of platform-operator",
-		Args:  cobra.MinimumNArgs(0),
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(version)
-		},
+	ctrl.SetLogger(zap.New(func(o *zap.Options) {
+		o.Development = true
+	}))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:             scheme,
+		MetricsBindAddress: metricsAddr,
+		LeaderElection:     enableLeaderElection,
+		Port:               9443,
+		SyncPeriod:         &watchInterval,
 	})
-
-		root.AddCommand(&cobra.Command{
-		Use:   "docs",
-		Short: "generate documentation",
-		Run: func(cmd *cobra.Command, args []string) {
-			err := doc.GenMarkdownTree(root, "docs")
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println("Documentation generated at: docs")
-		},
-	})
-
-	// root.PersistentFlags().StringArrayP("config", "c", []string{utils.GetEnvOrDefault("PLATFORM_CONFIG", "config.yml")}, "Path to config file")
-	// root.PersistentFlags().StringArrayP("extra", "e", nil, "Extra arguments to apply e.g. -e ldap.domain=example.com")
-	root.PersistentFlags().CountP("loglevel", "v", "Increase logging level")
-	// root.PersistentFlags().Bool("dry-run", false, "Don't apply any changes, print what would have been done")
-	root.SetUsageTemplate(root.UsageTemplate() + fmt.Sprintf("\nversion: %s\n ", version))
-
-	if err := root.Execute(); err != nil {
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
 
+	err = cleanup.Add(mgr)
+	if err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Cleanup")
+		os.Exit(1)
+	}
+
+	// +kubebuilder:scaffold:builder
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
 }
---
+++ pkg/controllers/cleanup/cleanup_controller.go
@@ -0,0 +1,114 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cleanup
+
+import (
+	"context"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/selection"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("cleanup-controller")
+
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileCleanup{Client: mgr.GetClient(), Scheme: mgr.GetScheme()}
+}
+
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	c, err := controller.New("cleanup-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	c.Watch(&source.Kind{Type: &corev1.Namespace{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type ReconcileCleanup struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;create;update;patch;delete
+
+func (r *ReconcileCleanup) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	ctx := context.Background()
+
+	namespaces := corev1.NamespaceList{}
+
+	expireLabel, err := labels.NewRequirement("auto-delete", selection.Exists, []string{})
+	if err != nil {
+		log.Error(err, "Failed to build new requirement")
+		return reconcile.Result{}, err
+	}
+
+	expireLabelSelector := labels.NewSelector().Add(*expireLabel)
+	expireLabelListOption := client.MatchingLabelsSelector{Selector: expireLabelSelector}
+
+	err = r.List(ctx, &namespaces, expireLabelListOption)
+	if err != nil {
+		log.Error(err, "Failed to list namespaces")
+		return reconcile.Result{}, client.IgnoreNotFound(err)
+	}
+
+	for _, ns := range namespaces.Items {
+		if ns.Status.Phase == corev1.NamespaceTerminating {
+			log.V(1).Info("namespace is already terminating", "namespace", ns.Name)
+			continue
+		}
+
+		expiry := ns.Labels["auto-delete"]
+
+		duration, err := time.ParseDuration(expiry)
+		if err != nil {
+			log.Error(err, "Invalid duration for namespace", "namespace", ns.Name, "expiry", expiry)
+			continue
+		}
+
+		expiresOn := ns.GetCreationTimestamp().Add(duration)
+		if expiresOn.Before(time.Now()) {
+			log.V(1).Info("Deleting namespace", "namespace", ns.Name)
+
+			err = r.Delete(ctx, &ns)
+			if err != nil {
+				log.Error(err, "Failed to delete namespace", "namespace", ns.Name)
+				continue
+			}
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
